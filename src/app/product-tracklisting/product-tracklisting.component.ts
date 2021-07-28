b49261d30b0c2b43dffa38b9af9c138e600c24bd
Annamalaikathireshan <annamalaikathiresan@gmail.com>
2021-07-28 08:28:32 +0530

    module 5 update

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -11,13 +11,11 @@ export class ProductTracklistingComponent implements OnInit {
 
   albumInfo: Album;
 
-  constructor(private _productService = ProductService) {
-    
-   }
+  constructor(private _productService : ProductService) { }
 
   ngOnInit() {
 
-    
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
