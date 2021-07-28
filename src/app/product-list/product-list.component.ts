1c263f3b2ed81dfd9916a75d4687b7001ea8b4de
Annamalaikathireshan <annamalaikathiresan@gmail.com>
2021-07-28 08:54:44 +0530

    Update product-list.component.ts

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from "../product.service";
 })
 export class ProductListComponent implements OnInit {
 
-  products = [];
+  products: Product[];
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
