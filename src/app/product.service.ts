1ee2f9930c3ca069c784d453a2bcb842c54297c2
Annamalaikathireshan <annamalaikathiresan@gmail.com>
2021-07-28 07:39:10 +0530

    module 4

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { Album } from "./album";
 
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
